e5adb5263d2fc5d1535ff7287246b3ad34b8eb5b
Eloagbawe <eloagbawe@gmail.com>
2022-11-14 23:23:38 +0100

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This script makes a request to a REST API"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_data = requests.get("{}/users/{}".format(base_url, id)).json()
+    todo_data = requests.get("{}/users/{}/todos".format(base_url, id)).json()
+    name = user_data.get('username')
+
+    for value in todo_data:
+        data = '"{}","{}","{}","{}"\n'.format(id,
+                                              name,
+                                              value.get('completed'),
+                                              value.get('title'))
+        filename = "{}.csv".format(id)
+        with open(filename, 'a', encoding="utf-8") as f:
+            f.write(data)
