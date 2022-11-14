0d1c086ca9c07726151f6c47eca86568e8514ff4
Eloagbawe <eloagbawe@gmail.com>
2022-11-15 00:12:47 +0100

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This script makes a request to a REST API"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_data = requests.get("{}/users/".format(base_url)).json()
+
+    data_dict = {}
+    for value in user_data:
+        data = []
+        name = value.get('username')
+        id = value.get('id')
+        todo_data = requests.get("{}/users/{}/todos"
+                                 .format(base_url, id)).json()
+        for value in todo_data:
+            res_dict = {}
+            res_dict['username'] = name
+            res_dict['task'] = value.get('title')
+            res_dict['completed'] = value.get('completed')
+            data.append(res_dict)
+        data_dict[id] = data
+
+    filename = "todo_all_employees.json"
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(json.dumps(data_dict))
